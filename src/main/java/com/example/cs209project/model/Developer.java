71fde7fb49a34c66412dd4be136d846a34703b02
Bon-Bon <1157264534@qq.com>
2022-12-19 18:17:43 +0800

    Update table

--- src/main/java/com/example/cs209project/model/Developer.java
+++ src/main/java/com/example/cs209project/model/Developer.java
@@ -1,9 +1,6 @@
 package com.example.cs209project.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table
