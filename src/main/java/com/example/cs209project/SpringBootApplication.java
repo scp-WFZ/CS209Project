026281dd402b64cdb2d616775dec2244ef41991a
Bon-Bon <1157264534@qq.com>
2022-12-19 19:56:20 +0800

    Update SpringBootApplication.java

--- src/main/java/com/example/cs209project/SpringBootApplication.java
+++ src/main/java/com/example/cs209project/SpringBootApplication.java
@@ -1,12 +1,22 @@
 package com.example.cs209project;
 
+import com.example.cs209project.service.DataService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(DataService service){
+		return args -> {
+			service.addEntries();
+		};
 	}
 
 }
