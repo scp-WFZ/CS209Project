e9f08c829d2404e713a2fe3ffaa64a983c557ae3
Bon-Bon <1157264534@qq.com>
2022-12-19 17:30:01 +0800

    Update Issue.java

--- src/main/java/com/example/cs209project/model/Issue.java
+++ src/main/java/com/example/cs209project/model/Issue.java
@@ -11,12 +11,20 @@ import java.util.Date;
 public class Issue {
     @Id
     private Long id;
+    private Long repo_id;
     private String title;
     private String state;
     private Date open_date;
     private Date close_date;
-    private Long repo_id;
-    private Long user_id;
+
+    public Issue(Long id, Long repo_id, String title, String state, Date open_date, Date close_date) {
+        this.id = id;
+        this.repo_id = repo_id;
+        this.title = title;
+        this.state = state;
+        this.open_date = open_date;
+        this.close_date = close_date;
+    }
 
     public Issue() {
     }
@@ -25,27 +33,47 @@ public class Issue {
         return id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public Date getOpen_date() {
-        return open_date;
-    }
-
-    public Date getClose_date() {
-        return close_date;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getRepo_id() {
         return repo_id;
     }
 
-    public Long getUser_id() {
-        return user_id;
+    public void setRepo_id(Long repo_id) {
+        this.repo_id = repo_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getOpen_date() {
+        return open_date;
+    }
+
+    public void setOpen_date(Date open_date) {
+        this.open_date = open_date;
+    }
+
+    public Date getClose_date() {
+        return close_date;
+    }
+
+    public void setClose_date(Date close_date) {
+        this.close_date = close_date;
     }
 }
