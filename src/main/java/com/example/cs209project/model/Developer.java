63497dbddab1a59c316d90a439cf658a803eb1bc
Bon-Bon <1157264534@qq.com>
2022-12-19 17:29:59 +0800

    Update Developer.java

--- src/main/java/com/example/cs209project/model/Developer.java
+++ src/main/java/com/example/cs209project/model/Developer.java
@@ -10,7 +10,14 @@ import javax.persistence.Table;
 public class Developer {
     @Id
     private Long id;
+    private Long repo_id;
     private String name;
+
+    public Developer(Long id, Long repo_id, String name) {
+        this.id = id;
+        this.repo_id = repo_id;
+        this.name = name;
+    }
 
     public Developer() {
     }
@@ -19,7 +26,23 @@ public class Developer {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRepo_id() {
+        return repo_id;
+    }
+
+    public void setRepo_id(Long repo_id) {
+        this.repo_id = repo_id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
