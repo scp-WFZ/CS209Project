b001a42be750708d52e932bdc5b9feee8904a895
Bon-Bon <1157264534@qq.com>
2022-12-19 17:30:03 +0800

    Update Release.java

--- src/main/java/com/example/cs209project/model/Release.java
+++ src/main/java/com/example/cs209project/model/Release.java
@@ -11,10 +11,20 @@ import java.util.Date;
 public class Release {
     @Id
     private Long id;
-    private Long author_id;
     private Long repo_id;
+    private Long author_id;
+    private String name;
     private Date create_date;
     private Date publish_date;
+
+    public Release(Long id, Long repo_id, Long author_id, String name, Date create_date, Date publish_date) {
+        this.id = id;
+        this.repo_id = repo_id;
+        this.author_id = author_id;
+        this.name = name;
+        this.create_date = create_date;
+        this.publish_date = publish_date;
+    }
 
     public Release() {
     }
@@ -23,19 +33,47 @@ public class Release {
         return id;
     }
 
-    public Long getAuthor_id() {
-        return author_id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getRepo_id() {
         return repo_id;
     }
 
+    public void setRepo_id(Long repo_id) {
+        this.repo_id = repo_id;
+    }
+
+    public Long getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(Long author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Date getCreate_date() {
         return create_date;
     }
 
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
     public Date getPublish_date() {
         return publish_date;
+    }
+
+    public void setPublish_date(Date publish_date) {
+        this.publish_date = publish_date;
     }
 }
